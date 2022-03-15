67ac6c3e219305b3cbb8bf410c6e6ddbb09cacba
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-03-14 23:44:06 -0400

    hard hat deployment works!

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const { Conflux } = require('js-conflux-sdk');
+const { abi, bytecode } = require("../artifacts/contracts/Blog.sol/Blog.json");
+
+const mainnet = " https://main.confluxrpc.com";
+const testnet = "https://test.confluxrpc.com";
+async function main() {
+  const cfx = new Conflux({
+    url: testnet,
+    networkId: 1,
+    logger: console,
+  });
+
+  const acct = cfx.wallet.addPrivateKey("PRIVATE KEY");
+
+  const contract = cfx.Contract({ abi, bytecode });
+
+  const txReceipt = await contract.constructor("myBlog").sendTransaction({from:acct}).executed()
+  console.log(txReceipt);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
